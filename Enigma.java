4ff3fbf6dba7059cb6360e189068317720c9c626
Ahmadelsaw <ahmade@gwmail.gwu.edu>
2025-09-26 18:37:15 -0400

    Final fix

--- Enigma.java
+++ Enigma.java
@@ -1,76 +1,66 @@
-public class Enigma{
+public class Enigma {
 
     private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
-        "#EJOTYCHMRWAFKPUZDINSXBGLQV",
-        "#BDFHJLNPRTVXZACEGIKMOQSUWY",
-        "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
-        "#TGOWHLIFMCSZYRVXQABUPEJKND"};
+            "#EJOTYCHMRWAFKPUZDINSXBGLQV",
+            "#BDFHJLNPRTVXZACEGIKMOQSUWY",
+            "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
+            "#TGOWHLIFMCSZYRVXQABUPEJKND"};
 
-    private static final String ALPHA = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // fixed inner ring
+    private Rotor[] rotors;
 
-    private Rotor rotors[];
-        
     public Enigma(int id1, int id2, int id3, String start){
-
+    
         rotors = new Rotor[3];
-        rotors[0] = new Rotor(rotorInit[id3-1], start.charAt(0));
-        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
-        rotors[2] = new Rotor(rotorInit[id1-1], start.charAt(2));
-        
+        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0)); // Inner
+        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1)); // Middle
+        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2)); // Outer
     }
 
 
-    public String decrypt(String message){        
+    public String encrypt(String message) {
         StringBuilder out = new StringBuilder();
-        for(int k = 0; k < message.length(); k++){
-            char c = message.charAt(k);
-            if (c == '#') {
-                out.append('#');
-            } else {
-                int j2 = rotors[2].indexOf(c);// map back through inner rotor
-                char m2= ALPHA.charAt(j2);// map back through outer rotor
-                int j1 = rotors[1].indexOf(m2);
-                char m1= ALPHA.charAt(j1);
-                int j0 = rotors[0].indexOf(m1);
-                char outChar = ALPHA.charAt(j0);// get final char
-                out.append(outChar);// append to output
-            }
-            rotate(); // rotate rotors after each letter
+
+        for (char c : message.toCharArray()) {  // Loop over every character in the string
+
+            int innerIndex = rotors[0].indexOf(c); // Get index of character from inner ring
+            char outerChar1 = rotors[2].charAt(innerIndex); // Match to new character in outer ring
+
+            int middleIndex = rotors[1].indexOf(outerChar1); // Get index of character from middle ring
+            char finalChar = rotors[2].charAt(middleIndex); // Match to final encrypted character in the outer ring
+
+            // Write char and rotate
+            out.append(finalChar);
+            rotate();
         }
 
-            return out.toString();
-        }
+        return out.toString();
+    }
 
-
-    
-    public String encrypt(String message){
+    public String decrypt(String message) {
         StringBuilder out = new StringBuilder();
-        for(int k = 0; k < message.length(); k++){
-            char c = message.charAt(k);
-            if (c == '#') {
-                out.append('#');// keep spaces as is
-            } else {
-                int i0 = ALPHA.indexOf(c);
-                char m1= rotors[0].charAt(i0);
-                int i1 = ALPHA.indexOf(m1);
-                char m2= rotors[1].charAt(i1);
-                int i2 = ALPHA.indexOf(m2);
-                char outChar = rotors[2].charAt(i2);
-                out.append(outChar);
+
+        for (char c : message.toCharArray()) { // Loop over every character in the string
+
+            int outerIndex1 = rotors[2].indexOf(c); // Find the index of the character on the outer ring
+            char middleChar = rotors[1].charAt(outerIndex1); // Get character in the middle Ring
+
+            int outerIndex2 = rotors[2].indexOf(middleChar); // Find the second (middleChar) index in the outer ring
+            char finalChar = rotors[0].charAt(outerIndex2); // Get final unencrypted char from the inner ring
+
+            // Write char and rotate
+            out.append(finalChar);
+            rotate();
+        }
+
+        return out.toString();
+    }
+
+    private void rotate(){
+        if(rotors[0].rotate()){ // Rotates inner ring
+            if(rotors[1].rotate()){ // Rotate middle if inner loops
+                rotors[2].rotate(); // Rotate outer if middle loops
             }
-            rotate(); // rotate rotors first
-        }
-
-            return out.toString();
-    }
-
-    
-private void rotate(){
-    if (rotors[0].rotate()) {      // step RIGHT rotor every lette
-        if (rotors[1].rotate()) {  // carry to MIDDLE if right says true
-            rotors[2].rotate();    // carry to LEFT if middle says true
         }
     }
-}
-    
+
 }
